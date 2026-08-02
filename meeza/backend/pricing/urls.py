f941b5ce7e561612c3b599bb28f25935cde0e7a7
Ziad-cpu-hue <code37265@gmail.com>
2026-08-02 14:55:31 +0300

    Delete meeza/backend directory

--- meeza/backend/pricing/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import CurrentPricingView, EstimatePriceView
-
-urlpatterns = [
-    path("current/", CurrentPricingView.as_view(), name="pricing-current"),
-    path("estimate/", EstimatePriceView.as_view(), name="pricing-estimate"),
-]
--- meeza/backend/trips/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Trip
-
-
-@admin.register(Trip)
-class TripAdmin(admin.ModelAdmin):
-    list_display = ("id", "customer", "driver", "service_type", "status", "total_price", "created_at")
-    list_filter = ("status", "service_type")
-    search_fields = ("pickup_address", "dropoff_address", "customer__email", "driver__email")
